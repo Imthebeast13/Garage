ca6cb6f2351813d313eef8abd7be684a1f910803
Imthebeast13 <afirmino5@gmail.com>
2023-03-03 17:31:43 +0000

    teste 123456

--- GarageManager/src/main/main.java
+++ GarageManager/src/main/main.java
@@ -9,7 +9,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("TESTE 111");
+		System.out.println("TESTE");
 		
 		
 		Client cliente = new Client("Andre", 961237815);
